aad4dc912957ddc6bc0a11f57afec7781b123f4c
Mikeblanchard <76125201+Mikeblanchard@users.noreply.github.com>
2021-02-25 18:31:46 -0500

    App beginning.

--- app.py
+++ app.py
@@ -9,3 +9,33 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+from typing_extensions import runtime
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+## Set up Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+flask run 
